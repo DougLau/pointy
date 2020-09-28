28f6c92bb2524e50d6221e0cf6cc93254d54dcf9
Doug Lau <doug.p.lau@gmail.com>
2020-09-28 12:00:12 -0500

    Initial commit (taken from footile)

---
+++ README.md
@@ -0,0 +1,7 @@
+# pointy
+
+A minimal 2D graphics library in Rust.
+
+See the [documentation] for examples and API usage.
+
+[documentation]: https://docs.rs/pointy
---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+// lib.rs      Pointy crate.
+//
+// Copyright (c) 2020  Douglas P Lau
+//
+//! Pointy is a minimal 2D graphics library.
+#![warn(missing_docs)]
+#![warn(missing_doc_code_examples)]
+
+mod geom;
+
+pub use geom::{Pt, Transform};
