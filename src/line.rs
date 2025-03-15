0fd408eaf13eca6943258268aafefd613df0e810
Doug Lau <doug.p.lau@gmail.com>
2025-03-15 11:41:47 -0500

    Add Line::canonical method

--- src/line.rs
+++ src/line.rs
@@ -14,7 +14,7 @@ use serde::{Deserialize, Serialize};
 ///
 /// let line = Line::new((10.0, 15.0), (0.0, 2.0));
 /// ```
-#[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+#[derive(Clone, Copy, Debug, Eq, PartialEq)]
 #[cfg_attr(feature = "serde", derive(Serialize, Deserialize))]
 pub struct Line<F>
 where
@@ -40,6 +40,47 @@ where
         Self {
             p0: p0.into(),
             p1: p1.into(),
+        }
+    }
+
+    /// Make canonical line.
+    ///
+    /// Two canonical lines can be compared for equality.
+    pub fn canonical(self) -> Self {
+        // lines for `x = 0`, `x = 1`, `y = 0`, `y = 1`
+        let x0 = Line::new((F::zero(), F::zero()), (F::zero(), F::one()));
+        let x1 = Line::new((F::one(), F::zero()), (F::one(), F::one()));
+        let y0 = Line::new((F::zero(), F::zero()), (F::one(), F::zero()));
+        let y1 = Line::new((F::zero(), F::one()), (F::one(), F::one()));
+        match (self.intersection(x0), self.intersection(y0)) {
+            (Some(p0), Some(p1)) => {
+                // does the line pass through the origin?
+                if p0 == p1 {
+                    // yes, find point at `x = 1`
+                    match self.intersection(x1) {
+                        Some(p1) => Line::new(p0, p1),
+                        None => unreachable!(),
+                    }
+                } else {
+                    // no, these are the canonical points
+                    Line::new(p0, p1)
+                }
+            }
+            (Some(p0), None) => {
+                // horizontal; find point at `x = 1`
+                match self.intersection(x1) {
+                    Some(p1) => Line::new(p0, p1),
+                    None => unreachable!(),
+                }
+            }
+            (None, Some(p0)) => {
+                // vertical; find point at `y = 1`
+                match self.intersection(y1) {
+                    Some(p1) => Line::new(p0, p1),
+                    None => unreachable!(),
+                }
+            }
+            _ => unreachable!(),
         }
     }
 
@@ -120,5 +161,23 @@ mod test {
         assert_eq!(d.project((0.0, -5.0)), Pt::new(0.0, 0.0));
         assert_eq!(d.project((5.0, -5.0)), Pt::new(5.0, 0.0));
         assert_eq!(d.project((10.0, -5.0)), Pt::new(10.0, 0.0));
+    }
+
+    #[test]
+    fn canonical() {
+        let a = Line::new((0.0, 0.0), (1.0, 0.0));
+        assert_eq!(a, a.canonical());
+        let a1 = Line::new((0.0, 15.0), (37.0, 0.0));
+        assert_eq!(a1, a1.canonical());
+        let a2 = Line::new((23.0, 0.0), (0.0, -19.0));
+        assert_eq!(a2.canonical(), Line::new((0.0, -19.0), (23.0, 0.0)));
+        let h0 = Line::new((0.0, 0.0), (2.0, 0.0));
+        assert_eq!(h0.canonical(), Line::new((0.0, 0.0), (1.0, 0.0)));
+        let h1 = Line::new((0.0, 1.0), (5.0, 1.0));
+        assert_eq!(h1.canonical(), Line::new((0.0, 1.0), (1.0, 1.0)));
+        let v0 = Line::new((0.0, -1.0), (0.0, 1.0));
+        assert_eq!(v0.canonical(), Line::new((0.0, 0.0), (0.0, 1.0)));
+        let v1 = Line::new((1.0, -3.0), (1.0, 8.0));
+        assert_eq!(v1.canonical(), Line::new((1.0, 0.0), (1.0, 1.0)));
     }
 }
